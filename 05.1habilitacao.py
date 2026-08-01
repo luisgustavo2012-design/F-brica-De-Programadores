d4df21c6da678dacf050140418e5980751576383
luisgustavo2012-design <dasilvamoraesluisgustavo79@gmail.com>
2026-08-01 08:59:24 -0300

    Delete 05.1habilitacao.py

--- 05.1habilitacao.py
+++
@@ -1,9 +0,0 @@
-#Criando as variaveis 
-nome = input("qual e o seu nome? ")
-idade =int(input("qual e sua idade? "))
-
-#verificando condicao da idade 
-if idade >= 18:
-    print("maior de idade")
-else:
-    print("menor de idade")
